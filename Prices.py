5f1dbe4f97f2eaeaa285bdef333734136e1a719c
SpookyChuxk <SpookyChuckChillin@yahoo.com>
2024-09-12 20:12:16 -0500

    Update Prices.py
    This Python code prompts the user to enter their full name and a minimum price. It then iterates through a predefined list of prices, calculating the total sum of all prices and counting how many prices exceed the specified minimum price. Finally, it prints a greeting with the user’s name, the minimum price, the count of prices greater than the minimum price, and the total sum of all prices.

--- Prices.py
+++ Prices.py
@@ -1,3 +1,4 @@
+This Python code prompts the user to enter their full name and a minimum price. It then iterates through a predefined list of prices, calculating the total sum of all prices and counting how many prices exceed the specified minimum price. Finally, it prints a greeting with the user’s name, the minimum price, the count of prices greater than the minimum price, and the total sum of all prices.
 count = 0
 sum = 0
 full_name = input("Enter your full name: ")
